531fd7c88b5155188218e9f73937dd096bacc1fd
Miguel Batista <msb2@cesar.school>
2024-11-30 19:30:35 -0300

    updated docs for 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="msplanner-tools",
-    version="0.1.4",
+    version="0.1.5",
     description="Library to interact with Microsoft Planner via API Graph",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
